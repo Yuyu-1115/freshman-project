30410d4ef6db65743990e7d17a93eb1b1e86b630
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-05-02 13:04:05 +0800

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    java
+    application
+    id("org.javamodularity.moduleplugin") version "1.8.12"
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+group = "org.openjfx.freshproject"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.10.2"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(22)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainModule.set("org.openjfx.freshproject.freshmanproject")
+    mainClass.set("org.openjfx.freshproject.freshmanproject.HelloApplication")
+}
+
+javafx {
+    version = "22.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.web", "javafx.swing", "javafx.media")
+}
+
+dependencies {
+    implementation("org.controlsfx:controlsfx:11.2.1")
+    implementation("net.synedra:validatorfx:0.5.0") {
+        exclude(group = "org.openjfx")
+    }
+    implementation("org.kordamp.ikonli:ikonli-javafx:12.3.1")
+    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
+    implementation("eu.hansolo:tilesfx:21.0.3") {
+        exclude(group = "org.openjfx")
+    }
+    implementation("com.github.almasb:fxgl:17.3") {
+        exclude(group = "org.openjfx")
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jlink {
+    imageZip.set(layout.buildDirectory.file("/distributions/app-${javafx.platform.classifier}.zip"))
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "app"
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.openjfx.freshproject.freshmanproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.openjfx.freshproject.freshmanproject to javafx.fxml;
+    exports org.openjfx.freshproject.freshmanproject;
+}
