917f9cee987476ba9d96f338c9fac4993858cbaa
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2025-06-10 21:15:28 +0800

    feature/core-utils Remove LevelComponent from Enemy

--- src/main/java/org/ncu/fresh/core/entity/factory/EnemyFactory.java
+++ src/main/java/org/ncu/fresh/core/entity/factory/EnemyFactory.java
@@ -20,7 +20,6 @@ public class EnemyFactory implements EntityFactory {
                 .at(position)
                 .with(new HealthDoubleComponent(maxHealth))
                 .with(new HealthBarComponent())
-                .with(new LevelComponent())
                 .with(new EnemyComponent())
                 .viewWithBBox(imageView)
                 .collidable()
